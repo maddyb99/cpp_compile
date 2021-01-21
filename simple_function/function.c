688d57f5e90f1d072d7fa8e43c6bc7f8193370ef
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-01-21 14:54:37 +0530

    simple function

---
+++ simple_function/function.c
@@ -0,0 +1,13 @@
+// #include "function.h"
+int nCompletionStatus = 0;
+float add(float x, float y)
+{
+ float z = x + y;
+ return z;
+}
+float add_and_multiply(float x, float y)
+{
+ float z = add(x,y);
+ z *= MULTIPLIER;
+ return z;
+}
---
+++ simple_function/main.c
@@ -0,0 +1,11 @@
+#include "function.h"
+extern int nCompletionStatus = 0;
+int main(int argc, char* argv[])
+{
+ float x = 1.0;
+ float y = 5.0;
+ float z;
+ z = add_and_multiply(x,y);
+ nCompletionStatus = 1;
+ return 0;
+}
